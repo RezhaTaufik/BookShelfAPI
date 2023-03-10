874b895023957e4546f334391efec5e000ee7c01
RezhaTaufik <rezhataufik1234@gmail.com>
2023-03-10 20:28:38 +0700

    Port and NPM run start
    change port to 9000 and add npm run start to scripts

--- Bookshelf API/Bookshelf API/src/server.js
+++ Bookshelf API/Bookshelf API/src/server.js
@@ -1,17 +1,17 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
- 
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: 9000,
+    host: "localhost",
     routes: {
       cors: {
-        origin: ['*'],
+        origin: ["*"],
       },
     },
   });
-  
+
   server.route(routes);
 
   await server.start();
